e44cfc74a9be78ce079fb41541f8bed1a7b95a83
shanejonas <jonas.shane@gmail.com>
2019-03-29 23:25:32 -0700

    fix: link to ietf draft

--- bin/build.spec.sh
+++ bin/build.spec.sh
@@ -1,1 +1,1 @@
-kramdown-rfc2629 ./spec/SPEC.md > ./build/SPEC.xml && xml2rfc --html --pdf --text ./build/SPEC.xml
+kramdown-rfc2629 ./spec/SPEC.md > ./build/SPEC.xml && xml2rfc --v3 --html --pdf --text ./build/SPEC.xml
